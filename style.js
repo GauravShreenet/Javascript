ba5c3ab2b79d0bc9366d7ae90987a8f3b0c6e4d0
Gaurav Shreenet <gthakuri656@gmail.com>
2023-09-02 21:31:02 +1000

    checking expire date with javascript

--- style.js
+++ style.js
@@ -16,7 +16,12 @@ const checkExpire = () => {
     const expireDate = new Date();
     const daysRemaining = Math.floor(expireDate - currentDate) / (1000 * 60 * 60 * 24);
 
-    
+    if (daysRemaining < 0) {
+        const daysExpired = Math.abs(daysRemaining);
+        return `${foodItem.name} is expired ${daysExpired} days ago.`;
+    }else{
+        return `You have ${daysUntilExpiration} days to use ${foodItem.name}.`;
+    }
 
 }
 
