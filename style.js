f50af141d28db19b96bf91ef01fe36f294d0748a
Gaurav Shreenet <gthakuri656@gmail.com>
2023-09-02 21:17:31 +1000

    checking expire date with javascript

---
+++ style.js
@@ -0,0 +1,20 @@
+
+const food = [ 
+    {
+        name: "Beef",
+        currentDate: "2023-08-25"
+    },
+    
+    {
+        name: "Milk",
+        currentDate: "2023-09-15"
+    }
+
+]
+
+const checkExpire = () => {
+    const date1 = 
+}
+
+
+
